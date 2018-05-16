cea95117b3b87dca993ddf109ee693d8048068ab
Nemanja Nedeljkovic <nemanjan00@gmail.com>
2018-05-16 19:40:47 +0200

    Documentation

--- examples/counter.js
+++ examples/counter.js
@@ -3,7 +3,7 @@ const s = require("../");
 
 let counter = 0;
 
-s.run(15).times().every(15).second().function(() => {
-	console.log(counter++);
+s.run(15).times().every(15).seconds().and(1).times().every(2).seconds().function(() => {
+	console.log(counter += 2);
 });
 
